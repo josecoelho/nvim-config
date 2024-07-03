15b073c65127c2bfac31c0a24e22e952acc4d0b5
José Coelho <jose.coelho@palomagroup.com>
2024-07-03 14:25:59 +1200

    Enable auto clean on save

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -3,17 +3,15 @@ local options = {
     lua = { "stylua" },
     css = { "prettier" },
     html = { "prettier" },
-    typescript = {"prettier"},
-    javascript = {"prettier"},
+    typescript = {{ "prettierd", "prettier" }},
+    javascript = {{ "prettierd", "prettier" }},
     terraform = {"tflint"},
 
 
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
+  format_after_save = {
+    lsp_format = "fallback",
   },
 }
 
--- lua/options.lua
+++ lua/options.lua
@@ -1,7 +1,7 @@
 require "nvchad.options"
 
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+-- o.cursorlineopt ='both' -- to enable cursorline!
 o.number = true
 o.relativenumber = true
 
